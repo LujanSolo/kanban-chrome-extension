f1cfbbf8c73cac804df6359d49958d7393b42a21
LujanSolo <lujansolo@proton.me>
2024-02-02 18:09:18 -0800

    added code for Board.tsx

---
+++ src/components/Board.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material';
+
+const Board = () => {
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h4" gutterBottom>
+        Kanban Board - Chrome Extension
+      </Typography>
+    </Container>
+  )
+};
+
+export default Board;
