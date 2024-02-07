7c3404dab07a8f2efb64d5c7a8882f8b89ef38d2
LujanSolo <lujansolo@proton.me>
2024-02-07 13:16:36 -0800

    added customDragLayer component with draglayer functionality

--- src/components/CustomDragLayer.tsx
+++ src/components/CustomDragLayer.tsx
@@ -12,3 +12,53 @@ interface CollectedProps {
   item: DragItem | null;
   currentOffset: { x: number; y: number } | null;
 }
+
+const layerStyles: React.CSSProperties = {
+  position: 'fixed',
+  pointerEvents: 'none',
+  zIndex: 1000,
+  left: 0,
+  top: 0,
+};
+
+const getItemStyles = (currentOffset: XYCoord | null): React.CSSProperties => {
+  if (!currentOffset) {
+    return {
+      display: 'none',
+    };
+  }
+
+  const { x, y } = currentOffset;
+  const transform = `translate(${x}px, ${y}px)`;
+  return {
+    transform,
+    WebkitTransform: transform,
+  };
+};
+
+const CustomDragLayer: React.FC = () => {
+  const {
+    itemType,
+    isDragging,
+    item,
+    currentOffset,
+  } = useDragLayer((monitor): CollectedProps => ({
+    item: monitor.getItem(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging(),
+  }));
+
+  if (!isDragging || !item) {
+    return null;
+  }
+
+  return (
+    <div style={layerStyles}>
+      <div style={getItemStyles(currentOffset)}>
+        <CustomDragItem item={item} />
+      </div>
+    </div>
+  );
+};
+
+export default CustomDragLayer;
